ebba25f1645b4ecdc7b0381a45d3877ff3d63ddb
AngelaEspinosa <titagena@gmail.com>
2022-04-28 13:05:52 -0500

    CREATE: se logra crear la funcion anonima que crea y dibuja la pelota en el tablero.

--- main.js
+++ main.js
@@ -12,11 +12,26 @@
     self.Board.prototype = {
         get elements(){                     
             var elements = this.bars;
-           // elements.push(this.ball);
+           elements.push(this.ball);
             return elements;
         }
     }
 })(); 
+
+(function(){
+    self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius= radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle";
+    }
+})();
+
 //funcion anonima que nos crea las barras para el juego.
 (function(){
     self.Bar = function(x,y,width,height,board){
@@ -63,9 +78,15 @@
 
                 draw(this.ctx,el);
             };
+        },
+        play: function(){
+            this.clean();
+            this.draw();
         }
 
     }
+
+    
 
     //funcion encargada de dibujar los elementos.
     function draw(ctx, element){
@@ -73,7 +94,14 @@
             case "rectangle":
                 ctx.fillRect(element.x, element.y,element.width, element.height);
                 break;
+                case "circle":
+                    ctx.beginPath();
+                    ctx.arc(element.x, element.y, element.radius, 0,7);
+                    ctx.fill();
+                    ctx.closePath();
+                    break;
         }           
+        
     }
 })();
 
@@ -82,6 +110,7 @@ var bar = new Bar(20,100,40,100, board);
 var bar_2 = new Bar(735,100,40,100, board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas,board);
+var ball = new Ball(350,100,10,board);
 
 
 document.addEventListener("keydown", function(ev){
@@ -106,8 +135,8 @@ document.addEventListener("keydown", function(ev){
 window.requestAnimationFrame(controller);
 
 function controller(){
-    board_view.clean();
-    board_view.draw();
+    
+    board_view.play();
     window.requestAnimationFrame(controller);
 }
 
